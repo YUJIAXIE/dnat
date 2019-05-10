cb088820fcb2751889be9db2e914e3093d270f6b
Administrator <Administrator@CIKOOSOFT>
2019-05-10 17:54:22 +0800

    1

--- DNAT/Main.cs
+++ DNAT/Main.cs
@@ -120,11 +120,11 @@ namespace DNAT
                 Frpini.IniWriteValue(dr["MappingName"].ToString(), dr["Info"].ToString(), dr["Value"].ToString());
             }
             var dd = Application.StartupPath + "\\frp.exe";
-            RunCmd2(dd, Frpini.inipath);
+            RunCmd2(dd,Frpini.inipath);
 
         }
         
-        static bool RunCmd2(string cmdExe, string cmdStr)
+        static bool RunCmd2(string cmdExe,string cmdStr)
         {
             bool result = false;
             try
@@ -139,12 +139,11 @@ namespace DNAT
                     myPro.StartInfo.CreateNoWindow = true;
                     myPro.Start();
                     //如果调用程序路径中有空格时，cmd命令执行失败，可以用双引号括起来 ，在这里两个引号表示一个引号（转义）
-                    string str = cmdExe + " -c " + cmdStr;//+" &exit";
-                    //d:\dyc\frpc_windows_386.exe -c d:\dyc\dyc.ini
+                    string str = string.Format(@"""{0}"" -c {1} {2}", cmdExe, cmdStr, "&exit");
+
                     myPro.StandardInput.WriteLine(str);
                     myPro.StandardInput.AutoFlush = true;
                     myPro.WaitForExit();
-
                     result = true;
                 }
             }
