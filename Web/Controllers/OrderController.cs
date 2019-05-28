2f2bb5e3736a16fec562c00aedc97a2aab5c7310
谢豫佳 <@DESKTOP-OME0SV2>
2019-05-28 21:06:00 +0800

    12

--- Web/Controllers/OrderController.cs
+++ Web/Controllers/OrderController.cs
@@ -15,6 +15,7 @@ using System.Web.Mvc;
 using Web.Common;
 using ThoughtWorks.QRCode.Codec;
 using System.IO;
+using System.Threading;
 
 namespace Web.Controllers
 {
@@ -53,7 +54,7 @@ namespace Web.Controllers
             var datetime = DateTime.Now;
             PriceBLL pb = new PriceBLL();
             ConfigBLL cb = new ConfigBLL();
-            if ((int)pb.IdSelectPrice(price).Rows[0]["Price"] > 0)
+            if (Convert.ToInt32(pb.IdSelectPrice(price).Rows[0]["Price"]) > 0)
             {
                 Order o = new Order();
                 o.OrderId = orderId = datetime.ToString("yyyyMMddHHmmssfff");
@@ -62,13 +63,13 @@ namespace Web.Controllers
                 o.OrderExplain = "购买" + pb.IdSelectPrice(price).Rows[0]["Type"] + "云隧道";
                 o.CreateTime = datetime.ToString();
                 o.TradingStatus = "待付款";
-                IAopClient client = new DefaultAopClient(cb.SelectValue("serverUrlserverUrl"), cb.SelectValue("app_id"), cb.SelectValue("merchant_private_key"), "json", "1.0", "RSA2", cb.SelectValue("alipay_public_key"), "GBK", false);
+                IAopClient client = new DefaultAopClient(cb.SelectValue("serverUrl"), cb.SelectValue("appId"), cb.SelectValue("merchant_private_key"), "json", "1.0", "RSA2", cb.SelectValue("alipay_public_key"), "GBK", false);
                 AlipayTradePrecreateRequest request = new AlipayTradePrecreateRequest();
                 request.BizContent = "{" +
                 "\"out_trade_no\":\"" + o.OrderId + "\"," +
                 "\"total_amount\":" + pb.IdSelectPrice(price).Rows[0]["Price"] + "," +
                 "\"subject\":\"" + o.OrderExplain + "\"," +
-                "\"qr_code_timeout_express\":\"1h\"" +
+                "\"timeout_express\":\"1h\"" +
                 "  }";
                 AlipayTradePrecreateResponse response = client.Execute(request);
                 switch (response.Code)
@@ -78,6 +79,18 @@ namespace Web.Controllers
                         result = ob.InsertOrder(o);
                         break;
                 }
+            }
+            else
+            {
+                Order o = new Order();
+                o.OrderId = orderId = datetime.ToString("yyyyMMddHHmmssfff");
+                o.UserId = (int)Session["Id"];
+                o.PriceId = price;
+                o.OrderExplain = "购买" + pb.IdSelectPrice(price).Rows[0]["Type"] + "云隧道";
+                o.CreateTime = datetime.ToString();
+                o.TradingStatus = "已付款";
+                result = ob.InsertOrder(o);
+                ///这里修改免费版到期时间
             }
             if (result > 0)
                 return Content(orderId);
@@ -118,7 +131,69 @@ namespace Web.Controllers
         }
         public ActionResult DoPay(string code)
         {
+
+            //轮询订单结果
+            //根据业务需要，选择是否新起线程进行轮询
+            ParameterizedThreadStart ParStart = new ParameterizedThreadStart(LoopQuery);
+            Thread myThread = new Thread(ParStart);
+            object o = code;
+            myThread.Start(o);
+
             return View();
+        }
+
+        /// <summary>
+        /// 轮询
+        /// </summary>
+        /// <param name="o">订单号</param>
+        public void LoopQuery(object o)
+        {
+            ConfigBLL cb = new ConfigBLL();
+
+            int count = 100;
+            int interval = 10000;
+            IAopClient client = new DefaultAopClient(cb.SelectValue("serverUrl"), cb.SelectValue("appId"), cb.SelectValue("merchant_private_key"), "json", "1.0", "RSA2", cb.SelectValue("alipay_public_key"), "GBK", false);
+            AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+            request.BizContent = "{" +
+            "\"out_trade_no\":\"" + o.ToString() + "\"," +
+            "\"trade_no\":\"'\"," +
+            "  }";
+            for (int i = 1; i <= count; i++)
+            {
+                Thread.Sleep(interval);
+                AlipayTradeQueryResponse response = client.Execute(request);
+                //queryResult = serviceClient.tradeQuery(out_trade_no);
+                //if (queryResult != null)
+                //{
+                    if (response.TradeStatus == "TRADE_SUCCESS")
+                    {
+                        DoSuccessProcess(o.ToString());
+                        return;
+                    }
+                //}
+            }
+            DoFailedProcess(o.ToString());
+            
+            
+            
+  
+        }
+
+        /// <summary>
+        /// 请添加支付成功后的处理
+        /// </summary>
+        private void DoSuccessProcess(string no)
+        {
+            //支付成功，请更新相应单据
+        }
+        /// <summary>
+        /// 请添加支付失败后的处理
+        /// </summary>
+        private void DoFailedProcess(string no)
+        {
+
+            //支付失败，请更新相应单据
+            
         }
     }
 }
